43a716ec463a6951ce3ba8e38cf917c0a4c8b547
Adithya Viswanathan <adithya.viswanathan26@gmail.com>
2021-02-19 16:48:02 -0600

    get sunrise and sunset times

--- main.py
+++ main.py
@@ -34,4 +34,30 @@ df = pd.read_json(json_list)
 df['risetime'] = pd.to_datetime(df['risetime'], unit='s')
 df['risetime'] = df['risetime'] - datetime.timedelta(hours=6)
 df['duration'] /= 60
-print(df)
+#print(df)
+
+# Get today's sunrise and sunset times
+sun_query = "https://api.sunrise-sunset.org/json?lat=" + lat + "&lng=" + lon + "&date=today"
+
+req = requests.get(sun_query)
+obj = req.json()
+
+#print(type(obj))
+
+json_list = json.dumps(req.json()['results'])
+loaded_json_list = json.loads(json_list)
+#print(type(loaded_json_list))
+
+sunrise = (pd.to_datetime(loaded_json_list['sunrise']) - datetime.timedelta(hours=6))
+sunset = (pd.to_datetime(loaded_json_list['sunset']) - datetime.timedelta(hours=6))
+
+print(sunrise, sunset)
+
+acceptable_transit_morning = [(sunrise - datetime.timedelta(hours=2)).time(), sunrise.time()]
+acceptable_transit_evening = [sunset.time(), (sunset + datetime.timedelta(hours=2)).time()]
+print(acceptable_transit_morning, acceptable_transit_evening)
+
+# Eliminate transit times which are not one hour before sunrise and one hour after sunset
+
+
+# Send email with list of transit times
